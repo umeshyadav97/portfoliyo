64f44d7106eaab6ea262651eb51e2c6871459730
umesh-nickelfox <umesh@nickelfox.com>
2023-12-23 23:24:48 +0530

    Feat()/ add project section and contact page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -12,6 +12,7 @@ import ReactImage from "../../assets/images/icons/react.svg";
 import Next from "../../assets/images/icons/next.svg";
 import Mui from "../../assets/images/icons/mui.svg";
 import Tailwind from "../../assets/images/icons/tailwind.svg";
+import ProfileImage from "../../assets/images/backgrounds/profile-1.png"
 
 function Home() {
   const rotateAnimation = keyframes`
@@ -29,7 +30,7 @@ function Home() {
     { id: 1, src: HTML, alt: "Image 1" },
     { id: 2, src: CSS, alt: "Image 2" },
     { id: 3, src: JS, alt: "Image 3" },
-    { id: 4, src: React, alt: "Image 4" },
+    { id: 4, src: ReactImage, alt: "Image 4" },
     { id: 5, src: Next, alt: "Image 5" },
     { id: 6, src: Mui, alt: "Image 6" },
     { id: 7, src: Tailwind, alt: "Image 7" },
@@ -91,7 +92,7 @@ function Home() {
           </Box>
           <Box xs={6} style={{ position: "relative", overflow: "hidden" }}>
             <Image
-              src="/_next/static/media/profile-1.2359b116.png"
+              src={ProfileImage}
               width={330}
               height={330}
               priority
